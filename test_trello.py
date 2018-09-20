779875623c906e40801eb6328e1751795e6b6ef3
Shivahari Pitchaikkannu <shivaharip@Shivaharis-MacBook-Pro.local>
2018-09-20 23:27:10 +0530

    Modifed the test file to reflect the changes made to the trello class

--- test_trello.py
+++ test_trello.py
@@ -6,8 +6,6 @@ from conf import key,token
 
 def test_trello(key,token):
     test_obj = trello(key,token)
-    test_obj.get_account_details()
-    test_obj.get_board_card_details()
 
 if __name__ == '__main__':
     test_trello(key=key,token=token)
